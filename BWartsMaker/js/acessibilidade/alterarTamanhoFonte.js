109f52b20de8b16df541817e461c3007f483c1cc
Guilherme Brito Dos Santos <guidobritosantosss@gmail.com>
2026-04-22 15:34:34 -0300

    fix(configurações): seletor global
    Adicionei o seletor global <html> para mudar o tamanho da fonte de todos os campos.

--- BWartsMaker/js/acessibilidade/alterarTamanhoFonte.js
+++ BWartsMaker/js/acessibilidade/alterarTamanhoFonte.js
@@ -1,21 +1,23 @@
 // Selecionando todos os inputs com o name Tamanho_fonte
 const selecaoTamanhoFonte = document.querySelectorAll('input[name="tamanho_fonte"]');
+// Seleciona todos os documentos html
+document.documentElement = "html";
 
 // Percorrendo os inputs e verificando qual está selecionado
 selecaoTamanhoFonte.forEach(opc => {
     opc.addEventListener("click", () => {
         if(opc.value === "tamanho_padrao"){
-            document.body.style.fontSize = "16px";
+            document.documentElement.style.fontSize = "16px";
             console.log("padrao");
         }
         
         if(opc.value === "tamanho_grande"){
-            document.body.style.fontSize = "24px";
+            document.documentElement.style.fontSize = "24px";
             console.log("big");
         }   
 
         if(opc.value === "tamanho_extra_grande"){
-            document.body.style.fontSize = "32px";
+            document.documentElement.style.fontSize = "32px";
             console.log("bigbig");
         }
     });
