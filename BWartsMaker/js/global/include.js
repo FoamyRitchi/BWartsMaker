fd03533eaf9be546a60c251de3f2a9599eefad86
Pedypowgui <guidobritosantosss@gmail.com>
2026-05-11 10:49:09 -0300

    Feat: Erro no include
    Ajustei o include.js para realizar a indexagem relativa a cada documento que chama o include.

--- BWartsMaker/js/global/include.js
+++ BWartsMaker/js/global/include.js
@@ -1,11 +1,32 @@
+function getRelativePart() {
+    const path = window.location.pathname;
+    const pagesIndex = path.indexOf('/pages/');
+
+    if (pagesIndex === -1) {
+        return '';
+    }
+
+    const afterPages = path.slice(pagesIndex + '/pages/'.length);
+    const depth = afterPages.split('/').length - 1;
+    return '../'.repeat(depth);
+}
+
+function normalizeResourcePaths(html) {
+    const base = getRelativePart();
+    return html.replace(/(href|src)="(styles\/[^"']+|header\/[^"']+|footer\/[^"']+)"/g, (match, attr, value) => {
+        return `${attr}="${base}${value}"`;
+    });
+}
+
 // Função para incluir o head nas próximas páginas
 function incluirHead() {
-    // Chama o arquivo head.html para as páginas
-    fetch('head.html')
+    const headPath = getRelativePart() + 'head.html';
+
+    fetch(headPath)
         .then(response => response.text())
         .then(data => {
-            // Insere o conteúdo dentro do <head> existente
-            document.head.insertAdjacentHTML('beforeend', data);
+            const normalized = normalizeResourcePaths(data);
+            document.head.insertAdjacentHTML('beforeend', normalized);
         })
         // Mensagem de erro caso aconteça algo inesperado
         .catch(error => console.error('Erro ao carregar o head:', error));
