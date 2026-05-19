fc65a0443c8e8bb0ef43cadcc26ea3688f3a843b
Guilherme Brito Dos Santos <guidobritosantosss@gmail.com>
2026-05-19 08:56:14 -0300

    Feat(form_login): Verificação com JS
    Adicionei a verificação do email do usuário com JavaScript.

--- BWartsMaker/js/validacoes/validacaoLogin.js
+++ BWartsMaker/js/validacoes/validacaoLogin.js
@@ -1,35 +1,32 @@
+// Erro_login é para quando haver a verificação no banco de dados, se existe ou não o cadastro
+
 // Inputs do usuário
-let cpf_user = document.getElementById("cpf_user");
-let senha_user = document.getElementById("senha_user");
+let email_user = document.getElementById("email_user");
 
 // Erros de inputs
-let erro_cpf = document.getElementById("cpf_erro");
-let erro_senha = document.getElementById("senha_erro");
+let erro_email = document.getElementById("erro_email");
+// --- let erro_login = document.getElementById("erro_email");
+
+// Botões
+let botao_login = document.getElementById("botao_login");
 
 // "Retirar" o tamanho da div do texto
-erro_cpf.style.display = "none";
-erro_senha.style.display = "none";
+erro_email.style.display = "none";
+// --- erro_login.style.display = "none";
 
-// Permitir que o usuário digite apenas números para informar o CPF
-cpf_user.addEventListener("input", () => {
-    const cpfFiltrado = cpf_user.value.replace(/[^0-9]/g, "");
+// Verificar formato do email
+email_user.addEventListener("change", () => {
+    const regex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
 
-    if(cpf_user.value !== cpfFiltrado){
-        erro_cpf.style.display = "block";
-        erro_cpf.textContent = "ERRO: Digite apenas números para informar o CPF.";
+    if (!regex.test(email_user.value)) {
+        erro_email.style.display = "block";
+        erro_email.textContent = "ERRO: Email inválido, formato esperado: seuemail@example.com";
     } else {
-        erro_cpf.style.display = "none";
-    }
-
-    cpf_user.value = cpfFiltrado;
-});
-
-// Limitar o CPF a apenas 11 caracteres
-cpf_user.addEventListener("change", () => {
-    if(cpf_user.value.length !== 11){
-        erro_cpf.style.display = "block";
-        erro_cpf.textContent = "ERRO: CPF deve ter 11 números";
-    } else {
-        erro_cpf.style.display = "none";
+        erro_email.style.display = "none";
     }
 });
+
+// Redirecionar para página do perfil
+botao_login.addEventListener("click", () => {
+    window.location.href = "../../pages/perfil_visao_geral/user_visao_geral.html";
+})
