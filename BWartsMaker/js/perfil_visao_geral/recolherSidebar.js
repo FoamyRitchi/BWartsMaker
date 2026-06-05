b2e80bdbca059954ccc8675739c16e83ad7384d0
Guilherme Brito Dos Santos <guidobritosantosss@gmail.com>
2026-06-05 11:56:04 -0300

    Feat(Perfil): Visão geral - usuário
    Realizei a página de visão geral do perfil de usuário

---
+++ BWartsMaker/js/perfil_visao_geral/recolherSidebar.js
@@ -0,0 +1,48 @@
+// Elementos
+const sidebar = document.getElementById("sidebar");
+const botao_recolher_menu = document.getElementById("botao_recolher_menu");
+const sidebar_icone = document.getElementById("sidebar_icone");
+
+// Botões
+const sair_conta = document.getElementById("sair_conta");
+const cancelar_saida = document.getElementById("cancelar_saida");
+const fechar_modal = document.getElementById("fechar_modal");
+
+// Recolher sidebar
+botao_recolher_menu.addEventListener("click", () => {
+    sidebar.classList.toggle("recolhida");
+    atualizarIcone();
+});
+
+// Sair da conta
+sair_conta.addEventListener("click", () => {
+    window.location.href = "../forms/form_login.html";
+})
+
+// Cancelar saída da conta
+cancelar_saida.addEventListener("click", () => {
+    fechar_modal.dispatchEvent(new Event("click"));
+})
+
+
+// Animação do ícone de recolher menu
+function atualizarIcone() {
+    if (sidebar.classList.contains("recolhida")) {
+        sidebar_icone.className = "bi bi-chevron-right";
+    } else {
+        sidebar_icone.className = "bi bi-chevron-left";
+    }
+}
+
+// Recolher menu automaticamente quando atingir tela de tablet
+function recolherMenuAutomatico() {
+    if (window.innerWidth < 768) {
+        sidebar.classList.add("recolhida");
+    } else {
+        sidebar.classList.remove("recolhida");
+    }
+    atualizarIcone();
+}
+
+recolherMenuAutomatico();
+window.addEventListener("resize", recolherMenuAutomatico);
